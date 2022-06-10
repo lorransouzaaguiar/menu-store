e7a7307f009872f93910ef3a8536eb5cd8088829
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-06-10 16:42:30 +0000

    chore: add css support and ignore babelrc

--- menu-store-frontend/rollup.config.js
+++ menu-store-frontend/rollup.config.js
@@ -4,6 +4,11 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import image from '@rollup/plugin-image';
+import postcss from 'rollup-plugin-postcss';
+import json from "@rollup/plugin-json"
+
+const PORT = 3000
 
 export default {
   input: "src/index.js",
@@ -13,22 +18,30 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    image(),
+    postcss({
+      extensions: [".css"],
+    }),
     nodeResolve({
+      preferBuiltins: true, browser: true,
       extensions: [".js"],
     }),
+    json(),
     replace({
+      preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify( 'development' )
     }),
     babel({
       presets: ["@babel/preset-react"],
+      babelrc: false
     }),
     commonjs(),
     serve({
       open: true,
       verbose: true,
       contentBase: ["", "public"],
-      port: 8080,
+      port: PORT,
     }),
-    livereload({ watch: "dist" }),
+    livereload({ watch: "dist" })
   ]
 };
