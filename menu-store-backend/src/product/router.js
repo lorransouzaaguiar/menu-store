4153d587e750c6b5d83aecdd7af313806ac62db8
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-06-27 23:25:51 +0000

    feat: add product router

---
+++ menu-store-backend/src/product/router.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { productController } from '@Product/controller'
+
+const productsRouter = express.Router()
+const {
+    getByLimitOffset,
+    insertOne,
+    removeOne,
+    updateOne,
+    getByCategories,
+    getByCategory,
+} = productController()
+
+productsRouter.post('/', insertOne)
+productsRouter.route('/:id').delete(removeOne).put(updateOne)
+//productsRouter.get('/:limit/:offset', getByLimitOffset)
+//productsRouter.get('/categories/:limit/:offset', getByCategories)
+
+export { productsRouter }
