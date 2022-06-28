48917cdd51c8d94e734dcd611a9784ceb18c2594
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-06-28 02:12:37 +0000

    chore: add import alias

--- menu-store-frontend/rollup.config.js
+++ menu-store-frontend/rollup.config.js
@@ -1,47 +1,57 @@
-import serve from "rollup-plugin-serve";
-import livereload from "rollup-plugin-livereload";
-import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-import image from '@rollup/plugin-image';
-import postcss from 'rollup-plugin-postcss';
-import json from "@rollup/plugin-json"
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+import babel from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import replace from '@rollup/plugin-replace'
+import image from '@rollup/plugin-image'
+import postcss from 'rollup-plugin-postcss'
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias'
 
 const PORT = 3000
 
 export default {
-  input: "src/index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "iife",
-    sourcemap: true,
-  },
-  plugins: [
-    image(),
-    postcss({
-      extensions: [".css"],
-    }),
-    nodeResolve({
-      preferBuiltins: true, browser: true,
-      extensions: [".js"],
-    }),
-    json(),
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
-    }),
-    babel({
-      presets: ["@babel/preset-react"],
-      babelrc: false
-    }),
-    commonjs(),
-    serve({
-      open: true,
-      verbose: true,
-      contentBase: ["", "public"],
-      port: PORT,
-    }),
-    livereload({ watch: "dist" })
-  ]
-};
+    input: 'src/index.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        sourcemap: true,
+    },
+    plugins: [
+        alias({
+            entries: [
+                { find: '@App', replacement: '../app/' },
+                { find: '@Cart', replacement: '../cart/' },
+                { find: '@Menu', replacement: '../menu/' },
+                { find: '@Shared', replacement: '../shared/' },
+            ],
+        }),
+        image(),
+        postcss({
+            extensions: ['.css'],
+        }),
+        nodeResolve({
+            preferBuiltins: true,
+            browser: true,
+            extensions: ['.js'],
+        }),
+        json(),
+        replace({
+            preventAssignment: true,
+            'process.env.NODE_ENV': JSON.stringify('development'),
+        }),
+        babel({
+            presets: ['@babel/preset-react'],
+            babelrc: false,
+        }),
+        commonjs(),
+        serve({
+            open: true,
+            verbose: true,
+            contentBase: ['', 'public'],
+            port: PORT,
+        }),
+        livereload({ watch: 'dist' }),
+    ],
+}
