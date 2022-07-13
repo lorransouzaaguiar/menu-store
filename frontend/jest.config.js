51c53d4d8704e74e52f0bd513d9507ab9884b9c5
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-07-13 17:10:41 +0000

    chore: add test match to modules cart and menu

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,16 @@
 export default {
     clearMocks: true,
-    testMatch: ['**/test/**/*.test.js'],
+    testMatch: [
+        '**/test/**/*.test.js',
+        '**/cart/**/test/*.test.js',
+        '**/menu/**/test/*.test.js',
+    ],
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '^@root(.*)$': '<rootDir>/src$1',
         '^@Menu(.*)$': '<rootDir>/src/menu$1',
+        '^@Cart(.*)$': '<rootDir>/src/cart$1',
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     },
+    setupFilesAfterEnv: ['./src/test/setupTests.js'],
 }
