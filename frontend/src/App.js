44105e96b96adddb932791f742f425a733a02f8a
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-07-21 18:42:04 +0000

    refactor: delegates the responsibility of managing the routes to the AppRoute component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,25 @@
 import React from 'react'
-import style from './App.module.css'
+import styles from './App.module.css'
 import MenuPage from '@Menu/page/MenuPage/MenuPage.js'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ProductDetailsPage from '@Menu/page/ProductDetailsPage/ProductDetailsPage.js'
 import OnSalePage from '@Menu/page/OnSalePage/OnSalePage'
 import CartPage from '@Cart/page/CartPage/CartPage'
 import CartDeliveryPage from '@Cart/page/CartDeliveryPage/CartDeliveryPage.js'
+import SignUpAdressPage from '@Cart/page/SignUpAdressPage/SignUpAdressPage'
+import { CartProvider } from '@Cart/context/cart-context'
+import { MenuProvider } from '@Menu/context/menu-context'
+import AppRoute from '@App/components/AppRoute/AppRoute'
 
 function App() {
     return (
-        <BrowserRouter>
-            <div className={style.app}>
-                <Routes>
-                    <Route path="/" element={<MenuPage />} />
-                    <Route path="/onSale" element={<OnSalePage />} />
-                    <Route path="/cart" element={<CartPage />} />
-                    <Route path="/delivery" element={<CartDeliveryPage />} />
-                    <Route path="/products" element={<ProductDetailsPage />}>
-                        <Route
-                            path=":productId"
-                            element={<ProductDetailsPage />}
-                        />
-                    </Route>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <MenuProvider>
+            <CartProvider>
+                <div className={styles.app}>
+                    <AppRoute />
+                </div>
+            </CartProvider>
+        </MenuProvider>
     )
 }
 export default App
---
+++ frontend/src/app/components/AppRoute/AppRoute.js
@@ -0,0 +1,24 @@
+import CartDeliveryPage from '@Cart/page/CartDeliveryPage/CartDeliveryPage'
+import CartPage from '@Cart/page/CartPage/CartPage'
+import SignUpAdressPage from '@Cart/page/SignUpAdressPage/SignUpAdressPage'
+import MenuPage from '@Menu/page/MenuPage/MenuPage'
+import OnSalePage from '@Menu/page/OnSalePage/OnSalePage'
+import ProductDetailsPage from '@Menu/page/ProductDetailsPage/ProductDetailsPage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function AppRoute() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MenuPage />} />
+                <Route path="/onSale" element={<OnSalePage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/delivery" element={<CartDeliveryPage />} />
+                <Route path="/signUpAdress" element={<SignUpAdressPage />} />
+                <Route path="/products" element={<ProductDetailsPage />}>
+                    <Route path=":productId" element={<ProductDetailsPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
