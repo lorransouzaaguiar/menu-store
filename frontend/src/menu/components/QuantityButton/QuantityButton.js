227b9dae213a555c8e02a40dc115efe7f0569030
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-07-21 20:59:42 +0000

    refactor: move QuantityButton component to menu context

---
+++ frontend/src/menu/components/QuantityButton/QuantityButton.js
@@ -0,0 +1,16 @@
+import { CartContext } from '@Cart/context/cart-context'
+import styles from './QuantityButton.module.css'
+
+export default function QuantityButton(props) {
+    return (
+        <div className={styles.container} style={{ ...props.style }}>
+            <button className={styles.button} onClick={props.decrementOnClick}>
+                -
+            </button>
+            <span className={styles.quantity}>{props.quantity}</span>
+            <button className={styles.button} onClick={props.incrementOnClick}>
+                +
+            </button>
+        </div>
+    )
+}
