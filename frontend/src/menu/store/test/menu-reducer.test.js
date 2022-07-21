3db9564db0d18882926709f3c1d14a6d490ee568
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-07-21 20:13:14 +0000

    test: refactor menu reducer test to new return from state

--- frontend/src/menu/store/test/menu-reducer.test.js
+++ frontend/src/menu/store/test/menu-reducer.test.js
@@ -5,16 +5,16 @@ const mockCategoriesApiData = [
         id: 1,
         description: 'molho',
         products: [
-            { id: 1, title: 'p1', description: '', price: 22 },
-            { id: 2, title: 'p2', description: '', price: 44 },
+            { id: 1, title: 'p1', description: '', price: 22.83 },
+            { id: 2, title: 'p2', description: '', price: 44.51 },
         ],
     },
     {
         id: 2,
         description: 'bebidas',
         products: [
-            { id: 1, title: 'p1', description: '', price: 12 },
-            { id: 2, title: 'p2', description: '', price: 33 },
+            { id: 1, title: 'p1', description: '', price: 12.6 },
+            { id: 2, title: 'p2', description: '', price: 33.3 },
         ],
     },
 ]
@@ -22,14 +22,14 @@ const mockCategoriesApiData = [
 describe('Testing Menu Reducer', () => {
     it('it should return the list of drinks and list of menu', async () => {
         const action = {
-            type: 'FETCH_CATEGORIES',
+            type: 'FETCH_MENU',
             payload: mockCategoriesApiData,
         }
         const state = menuReducer(menuState, action)
-        console.log(state)
-        expect(state.drinks.description).toBe('bebidas')
-        expect(state.drinks.products).toHaveLength(2)
-        expect(state.menu[0].description).toBe('molho')
-        expect(state.menu[0].products).toHaveLength(2)
+
+        expect(state.categories[0].description).toBe('molho')
+        expect(state.categories[0].products).toHaveLength(2)
+        expect(state.categories[1].description).toBe('bebidas')
+        expect(state.categories[1].products).toHaveLength(2)
     })
 })
