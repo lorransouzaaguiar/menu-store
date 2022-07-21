d4b99f78529a432b597d72f39a4d8365a9f63cde
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-07-21 20:07:49 +0000

    refactor: rename the hook to asyncActionFetchMenu

--- frontend/src/menu/store/async-actions.js
+++ frontend/src/menu/store/async-actions.js
@@ -1,7 +1,7 @@
 import { categoryRepository } from '@Menu/repository/category-repository.js'
 import { useFetch } from '@App/hooks/useFetch.js'
 
-export const useFetchCategories = (dispatch, action) => {
+export const asyncActionFetchMenu = (dispatch, action) => {
     const { getCategories } = categoryRepository()
     useFetch({ repositoryMethod: getCategories }, dispatch, action)
 }
